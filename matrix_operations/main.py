0eaadfd73295883a1e03624bc2fdf27c37c7992d
andris1177 <laszloandris1@gmail.com>
2024-05-24 21:23:24 +0200

    feladat 3

--- matrix_operations/main.py
+++ matrix_operations/main.py
@@ -1,4 +1,19 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+class MatrixOperations:
+    def __init__(self):
+       self.fileLocation = './input.txt'
+       self.data = [[]]
+       self.readFile()
+       self.teszt()
 
-print(input)
+    def readFile(self):
+        with open(self.fileLocation, "r") as f:
+            for i in f:
+                self.data.append(i.replace("\n", " ").strip().split())
+
+    def teszt(self):
+        for i in range(len(self.data)):
+            print(self.data[i])
+
+        
+
+solution = MatrixOperations()
