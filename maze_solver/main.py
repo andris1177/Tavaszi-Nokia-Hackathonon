9f06e702ff92f3eff84bd89f3da38f7c79446f74
andris1177 <laszloandris1@gmail.com>
2024-05-25 13:15:03 +0200

    matek 2

--- maze_solver/main.py
+++ maze_solver/main.py
@@ -1,4 +1,38 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+from re import compile, match 
 
-print(input)
+class MazeSolver:
+    def __init__(self):
+       self.fileLocation = "./input.txt"
+       self.mazes = {}
+       self.maze = []
+       self.mazeId = None
+       self.regexPatternRaw = r'^[A-Z]$'
+       self.regexPattern = compile(self.regexPatternRaw)
+
+    def readFile(self):
+        with open(self.fileLocation, 'r') as f:
+            for line in f:
+                line = line.replace(" ", "").strip()
+
+                if not line:
+                    continue
+
+                if self.regexPattern.match(line):
+                    if self.mazeId is not None:
+                        self.mazes[self.mazeId] = self.maze
+
+                    self.mazeId = line
+                    self.maze = []
+
+                else:
+                    self.maze.append(list(line))
+
+            if self.mazeId is not None:
+                self.mazes[self.mazeId] = self.maze
+
+    def run(self):
+        self.readFile()
+        print(self.mazes["A"])
+
+solution = MazeSolver()
+solution.run()
