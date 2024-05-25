ab165ef8ed3257072695644ce67d7d763580d3f8
andris1177 <laszloandris1@gmail.com>
2024-05-25 14:05:55 +0200

    matek 3 feladat

--- matrix_operations/main.py
+++ matrix_operations/main.py
@@ -1,19 +1,48 @@
+from re import compile, match
+
 class MatrixOperations:
     def __init__(self):
-       self.fileLocation = './input.txt'
-       self.data = [[]]
-       self.readFile()
-       self.teszt()
+        self.fileLocation = './input.txt'
+        self.matrixes = {}
+        self.matrix = []
+        self.matrixId = None
+        self.regexPatternRaw = r'^[A-Z]$'
+        self.regexPattern = compile(self.regexPatternRaw)
+        self.matriciesOrSolution = 0
 
     def readFile(self):
-        with open(self.fileLocation, "r") as f:
-            for i in f:
-                self.data.append(i.replace("\n", " ").strip().split())
+        with open(self.fileLocation, 'r') as f:
+            for line in f:
+                line = line.replace(" ", "").strip()
 
-    def teszt(self):
-        for i in range(len(self.data)):
-            print(self.data[i])
+                if line == "matrices" or self.matriciesOrSolution == 1:
+                    self.matriciesOrSolution = 1
+
+
+                    if not line:
+                        continue
+
+                    if self.regexPattern.match(line):
+                        if self.matrixId is not None:
+                            self.matrixes[self.matrixId] = self.matrix
+
+                        self.matrixId = line
+                        self.matrix = []
+
+                    else:
+                        self.matrix.append(list(line))
+
+                    if self.matrixId is not None:
+                        self.matrixes[self.matrixId] = self.matrix
+
+                else:
+                    return 
+
+    def run(self):
+        self.readFile()
+        print(self.matrixes)
 
         
 
 solution = MatrixOperations()
+solution.run()
